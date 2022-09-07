f66a1d4c1d50c5fd3ff272daf7ef1c43fcea5f6f
Alamin Shaikh <alaminshaikh@Alamins-MacBook-Pro.local>
2022-09-07 10:44:36 +0600

    Config updated

--- next.config.js
+++ next.config.js
@@ -3,15 +3,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    domains: [
-      process.env.WORDPRESS_API_URL.match(/(?!(w+)\.)\w*(?:\w+\.)+\w+/)[0],
-      "0.gravatar.com",
-      "1.gravatar.com",
-      "2.gravatar.com",
-      "secure.gravatar.com",
-    ],
-  },
 };
 
 module.exports = nextConfig;
